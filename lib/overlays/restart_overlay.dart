fa2f803d53895069462545c5c069944ff397bcb4
warioddly <warioddly@gmail.com>
2025-04-14 08:51:20 +0600

    New test design restart_overlay.dart

--- lib/overlays/restart_overlay.dart
+++ lib/overlays/restart_overlay.dart
@@ -11,15 +11,58 @@ class RestartOverlay extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.black54,
-      child: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            game.restart();
-          },
-          child: const Text('Restart'),
-        ),
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        color: Colors.black54,
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+              Text(
+                'Game Over',
+                style: const TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                ),
+              ),
+
+              const SizedBox(height: 20),
+
+              Text(
+                'Score: ${game.score}',
+                style: const TextStyle(
+                  fontSize: 22,
+                  color: Colors.white,
+                ),
+              ),
+
+              const SizedBox(height: 16),
+
+              IconButton(
+                onPressed: () {
+                  game.restart();
+                },
+                icon: const Icon(
+                  Icons.refresh,
+                  color: Colors.white,
+                  size: 40,
+                ),
+              ),
+
+              Text(
+                'Restart',
+                style: const TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
+              ),
+
+            ],
+          ),
+        )
       ),
     );
   }
